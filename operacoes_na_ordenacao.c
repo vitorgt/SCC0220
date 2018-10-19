eb7d9bdaa3ea6ef48859216b9cc39c35024b1029
Vitor Gratiere Torres <26410338+vitorgt@users.noreply.github.com>
2018-10-19 13:55:00 -0300

    Update operacoes_na_ordenacao.c

--- operacoes_na_ordenacao.c
+++ operacoes_na_ordenacao.c
@@ -4,13 +4,17 @@
 
 int data[5][2];// Cs and Ms of each sorting algorithm following IBMHQ (see printdata)
 
-void scanop(int o, char *line, char *op){
-	int i = 0, j = 0, len = strlen(line);
-	op[i++] = line[0];
-	if(o == 1) return;
-	for(j = 0; i < o && j < len; j++)
-		if(line[j] == ' ' && (j+1) < len)
-			op[i++] = line[j+1];
+void scanop(char *line, char *op){
+	int i = 0, len = strlen(line);
+	for(i = 0; i < len; i++){
+		if(i == 0 || (line[i] == ' ' && (i+1) < len)){
+			if(line[i] == 'I') op[0] = 1;
+			if(line[i] == 'B') op[1] = 1;
+			if(line[i] == 'M') op[2] = 1;
+			if(line[i] == 'H') op[3] = 1;
+			if(line[i] == 'Q') op[4] = 1;
+		}
+	}
 }
 
 void scanin(int n, int *in){
@@ -52,6 +56,7 @@ void I(int *v, int n){
 	for(; i < n; i++){
 		k = v[i];
 		j = i - 1;
+		data[0][0]++;
 		while(j >= 0 && v[j] > k){ data[0][0]++;
 			v[j+1] = v[j]; data[0][1]++;
 			j = j - 1;
@@ -63,15 +68,12 @@ void I(int *v, int n){
 void B(int *v, int n){
 	int i = 0, j = 0;
 	for(; i < n-1; i++)
-		for(j = 0; j < n-i-1; j++)
-			if(v[j] > v[j+1]){ data[1][0]++;
+		for(j = 0; j < n-i-1; j++){
+			data[1][0]++;
+			if(v[j] > v[j+1]){
 				swap(&v[j], &v[j+1]); data[1][1] += 2;
-				/*
-				int t = v[j];
-				v[j] = v[j+1]; data[1][1]++;
-				v[j+1] = t; data[1][1]++;
-				*/
 			}
+		}
 }
 
 void merge(int *v, int l, int m, int r){
@@ -86,10 +88,11 @@ void merge(int *v, int l, int m, int r){
 	i = j = 0;
 	k = l;
 	while(i < n1 && j < n2){
-		if(L[i] <= R[j]){ data[2][0]++;
+		data[2][0]++;
+		if(L[i] <= R[j]){
 			v[k++] = L[i++]; data[2][1]++;
 		}
-		else{ data[2][0]++;
+		else{
 			v[k++] = R[j++]; data[2][1]++;
 		}
 	}
@@ -113,16 +116,16 @@ void M(int *v, int l, int r){
 void heapify(int *v, int n, int i){
 	int largest = i;
 	int l = 2*i+1, r = 2*i+2;
-	if(l < n && v[l] > v[largest]){ data[3][0]++;
+	data[3][0]++;
+	if(l < n && v[l] > v[largest]){
 		largest = l;
 	}
-	if(r < n && v[r] > v[largest]){ data[3][0]++;
+	data[3][0]++;
+	if(r < n && v[r] > v[largest]){
 		largest = r;
 	}
 	if(largest != i){
-		int t = v[largest];
-		v[largest] = v[i]; data[3][1]++;
-		v[i] = t; data[3][1]++;
+		swap(&v[i], &v[largest]); data[3][1] += 2;
 		heapify(v, n, largest);
 	}
 }
@@ -132,70 +135,140 @@ void H(int *v, int n){
 	for(i = (n/2)-1; i >= 0; i--)
 		heapify(v, n, i);
 	for(i = n-1; i >= 0; i--){
-		int t = v[i];
-		v[i] = v[0]; data[3][1]++;
-		v[0] = t; data[3][1]++;
+		swap(&v[0], &v[i]); data[3][1] += 2;
 		heapify(v, i, 0);
 	}
 }
 
-int partition(int *v, int l, int r){
-	int pivot = v[(l+r)/2];
-	int i = (l-1), j = 0;
-	for(j = l; j <= r-1; j++){
-		if(v[j] <= pivot){
-			i++;
-			swap(&v[i], &v[j]);
-		}
-	}
-	swap(&v[i+1], &v[r]);
-	return (i+1);
-}
-
 void Q(int *v, int l, int r){
 	if(l < r){
-		int p = partition(v, l, r);
-		Q(v, l, p-1);
-		Q(v, p+1, r);
+		int L = l, R = r;
+		int pivot = v[(l+r)/2];
+		while(L <= R){
+			data[4][0]++;
+			while(v[L] < pivot){ data[4][0]++;
+				L++;
+			}
+			while(v[R] > pivot){ data[4][0]++;
+				R--;
+			}
+			if(L <= R){
+				swap(&v[L], &v[R]); data[4][1] += 2;
+				L++; R--;
+			}
+		}
+		Q(v, l, R);
+		Q(v, L, r);
 	}
+}
+
+char *menorC(char *op){
+	int p = data[0][0], d = 0, i = 0;
+	for(i = 0; i < 5; i++){
+		if(data[i][0] < p && op[i]){
+			p = data[i][0];
+			d = i;
+		}
+	}
+	if(d == 0) return "INSERTIONSORT";
+	if(d == 1) return "BUBBLESORT";
+	if(d == 2) return "MERGESORT";
+	if(d == 3) return "HEAPSORT";
+	if(d == 4) return "QUICKSORT";
+	else return "NONE";
+}
+
+char *maiorC(char *op){
+	int p = data[0][0], d = 0, i = 0;
+	for(i = 0; i < 5; i++){
+		if(data[i][0] > p && op[i]){
+			p = data[i][0];
+			d = i;
+		}
+	}
+	if(d == 0) return "INSERTIONSORT";
+	if(d == 1) return "BUBBLESORT";
+	if(d == 2) return "MERGESORT";
+	if(d == 3) return "HEAPSORT";
+	if(d == 4) return "QUICKSORT";
+	else return "NONE";
+}
+
+char *menorM(char *op){
+	int p = data[0][1], d = 0, i = 0;
+	for(i = 0; i < 5; i++){
+		if(data[i][1] < p && op[i]){
+			p = data[i][1];
+			d = i;
+		}
+	}
+	if(d == 0) return "INSERTIONSORT";
+	if(d == 1) return "BUBBLESORT";
+	if(d == 2) return "MERGESORT";
+	if(d == 3) return "HEAPSORT";
+	if(d == 4) return "QUICKSORT";
+	else return "NONE";
+}
+
+char *maiorM(char *op){
+	int p = data[0][1], d = 0, i = 0;
+	for(i = 0; i < 5; i++){
+		if(data[i][1] > p && op[i]){
+			p = data[i][1];
+			d = i;
+		}
+	}
+	if(d == 0) return "INSERTIONSORT";
+	if(d == 1) return "BUBBLESORT";
+	if(d == 2) return "MERGESORT";
+	if(d == 3) return "HEAPSORT";
+	if(d == 4) return "QUICKSORT";
+	else return "NONE";
 }
 
 int main(){
-	int o = 0, n = 0, in[30004], sort[30004];
+	int o = 0, n = 0, *in = NULL, *sort = NULL;
 	char op[5], *line = NULL;
-
-	for(; o < 5; o++)
-		op[o] = in[o] = sort[o] = 0;
-	for(; o < 30004; o++)
-		in[o] = sort[o] = 0;
 
 	scanf("%d %d\n", &o, &n);
 	scanf("%m[^\r\n]%*[\r\n]", &line);
-	scanop(o, line, op);
+	scanop(line, op);
 	free(line);
+
+	in = (int *)calloc(n, sizeof(int));
+	sort = (int *)calloc(n, sizeof(int));
 	scanin(n, in);
 
-	if(op[0] == 'I' || op[1] == 'I' || op[2] == 'I' || op[3] == 'I' || op[4] == 'I'){
+	if(op[0]){
 		copy_arr(n, in, sort);
 		I(sort, n);
 	}
-	if(op[0] == 'B' || op[1] == 'B' || op[2] == 'B' || op[3] == 'B' || op[4] == 'B'){
+	if(op[1]){
 		copy_arr(n, in, sort);
 		B(sort, n);
 	}
-	if(op[0] == 'M' || op[1] == 'M' || op[2] == 'M' || op[3] == 'M' || op[4] == 'M'){
+	if(op[2]){
 		copy_arr(n, in, sort);
 		M(sort, 0, n-1);
 	}
-	if(op[0] == 'H' || op[1] == 'H' || op[2] == 'H' || op[3] == 'H' || op[4] == 'H'){
+	if(op[3]){
 		copy_arr(n, in, sort);
 		H(sort, n);
 	}
-	if(op[0] == 'Q' || op[1] == 'Q' || op[2] == 'Q' || op[3] == 'Q' || op[4] == 'Q'){
+	if(op[4]){
 		copy_arr(n, in, sort);
 		Q(sort, 0, n-1);
 	}
-	printdata();
+
+	//printdata();
+
+	free(in);
+	free(sort);
+
+	printf("Menor C: %s\n", menorC(op));
+	printf("Maior C: %s\n", maiorC(op));
+	printf("Menor M: %s\n", menorM(op));
+	printf("Maior M: %s\n", maiorM(op));
 
 	return 0;
 }
